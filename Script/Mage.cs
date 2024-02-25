5033d3ed256f6bc8c20036cc6b8cc63976ea3b32
fieropiero <fieropiero@users.noreply.github.com>
2024-02-25 17:52:32 +0100

    added boss

---
+++ Script/Mage.cs
@@ -0,0 +1,40 @@
+using Godot;
+using System;
+
+public partial class Mage : Area2D
+{
+	public short hp = 500;
+	public Vector2 speedVec = new(0, 5);
+	public float direction = 1;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+
+	}
+
+	
+    public override void _PhysicsProcess(double delta)
+    {
+		Position += Transform.Y * speedVec.Y * direction;
+    }
+
+	public void _on_area_entered(Area2D area)
+	{
+		if(area.IsInGroup("Limit"))
+			direction *= -1;
+		else if(area.IsInGroup("Hat"))
+		{
+			hp--;
+		}
+		if(hp <= 0)
+		{
+			this.QueueFree();
+		}
+	}
+
+}
