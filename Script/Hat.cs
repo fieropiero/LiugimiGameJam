7c395f25d9f66c4106fcbb2d5e3fe787a8e5e1d7
fieropiero <fieropiero@users.noreply.github.com>
2024-02-25 19:22:50 +0100

    boomerang hat

--- Script/Hat.cs
+++ Script/Hat.cs
@@ -3,17 +3,39 @@ using System;
 
 public partial class Hat : Area2D
 {
-	public Vector2 speedVec = new(20, 0);
-	public float direction = 1;
+  public float speed = 20;
+	public Vector2 speedVec;
+	public float xdirection = 1;
+  private float toTravel = 400000;
+  private Vector2? startingPosition = null;
+  public Node2D target = null;
+  public bool boomerang = false;
 
-    public override void _PhysicsProcess(double delta)
-    {
-		Position += Transform.X * speedVec.X * direction;
-    }
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
+  public override void _PhysicsProcess(double delta)
+  {
+      if(boomerang)
+      {
+        homing();
+        return;
+      }
+		  Position += Transform.X * speedVec.X * xdirection;
+      if(Position.DistanceSquaredTo(startingPosition.Value) > toTravel)
+      {
+        boomerang = true;
+      }
+  }
+
+  public void homing()
+  {
+    Vector2 direction = Position.DirectionTo(target.Position);
+    Position += direction * speed;
+  }
+
+  public override void _Ready()
 	{
+    startingPosition = new(Position.X, Position.Y);
+    speedVec = new(speed, 0);
 	}
 
 }
