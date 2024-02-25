f48fa97304af58287eed0137cf78d3f9e0cfbdc7
fieropiero <fieropiero@users.noreply.github.com>
2024-02-25 19:50:35 +0100

    fixxettini

--- Script/Mage.cs
+++ Script/Mage.cs
@@ -4,7 +4,8 @@ using System;
 public partial class Mage : Area2D
 {
 	AnimationPlayer anim;
-	public short hp = 5;
+	[Export]
+	public short hp = 10;
 	public Vector2 speedVec = new(0, 5);
 	public float direction = 1;
 	// Called when the node enters the scene tree for the first time.
